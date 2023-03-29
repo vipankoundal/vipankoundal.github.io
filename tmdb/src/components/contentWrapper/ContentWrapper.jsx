f15e602986d3dde30bd31be1b32c183f2c4067c2
Vipan <115575172+vipankoundal@users.noreply.github.com>
2023-03-29 08:31:34 +0530

    Delete tmdb directory

--- tmdb/src/components/contentWrapper/ContentWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import "./style.scss";
-
-const ContentWrapper = ({ children }) => {
-  return <div className="content-wrapper">{children}</div>;
-};
-
-export default ContentWrapper;
--- tmdb/src/pages/404/PageNotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.scss";
-
-const PageNotFound = () => {
-  return <div>page not found</div>;
-};
-
-export default PageNotFound;
